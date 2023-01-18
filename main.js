2af055b25cbb1c06eb54b67ee7fe7d42535ec175
Hannah Hukins <hannah.hukins@gmail.com>
2023-01-18 13:54:41 +0000

    first version

---
+++ main.js
@@ -0,0 +1,12 @@
+import fs from "fs";
+
+const contents = fs.readFileSync("test.txt");
+
+console.log(contents.toString());
+
+var emailPattern = /@softwire\.com/g;
+
+const found = contents.toString().match(emailPattern);
+
+console.log(found.length);
+
